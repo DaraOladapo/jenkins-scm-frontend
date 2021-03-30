67f09b8fb4df66334a32b799cf04f50531e4a789
Dara Oladapo <daraoladapo@outlook.com>
2021-03-30 14:49:06 +0000

    modified to relad

--- setup.sh
+++ setup.sh
@@ -6,12 +6,15 @@ sudo apt update
 sudo apt install -y nginx
 sudo cp nginx.conf /etc/nginx/nginx.conf
 
+
+
 # copy files to installation folder
 install_folder=/opt/bookshelve
 sudo mkdir -p ${install_folder}
 sudo rm -rf ${install_folder}/*
 sudo cp -r . ${install_folder}
 
+
 # reload nginx
-sudo systemctl stop nginx
+sudo systemctl reload nginx
 sudo systemctl start nginx
